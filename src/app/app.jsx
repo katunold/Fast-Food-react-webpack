06ea12e0b462ab9141bed94f208fd11f1baf3fce
Katumba Arnold <katunold94@gmail.com>
2019-02-07 06:26:45 +0300

    ch(setup): setup webpack
    - setup webpack
    - setup up testing environment
    - implement sign-up and login form component
    - implement a nav-bar component
    
    [#Finishes #163754209 #163754379 #163754437 #163754506 #163754575]

---
+++ src/app/app.jsx
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+import NavBar from "../js/components/presentational/navBar/index.jsx";
+
+const App = () => (
+  <Fragment>
+      <NavBar/>
+  </Fragment>
+);
+
+export default App;
